1e06d48507f6bfd6c53b2e1b79990a67866bda2a
mp <maulikk95@gmail.com>
2022-03-18 15:22:59 -0400

    added tagtemplates

--- src/template/tag-template.js
+++ src/template/tag-template.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import RecipesList from '../components/RecipesList';
 
-const TagTemplate = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <h2>tag template</h2>
+const TagTemplate = ({ data }) => {
+  const recipes = data.allContentfulRecipe.nodes;
+  return (
+    <Layout>
+      <main className='page'>
+        <h2>tag name</h2>
+        <div className='tag-recipes'>
+          <RecipesList recipes={recipes} />
         </div>
-    )
+      </main>
+
+    </Layout>
+  )
 }
 
+export const query = graphql`
+ query GetRecipeByTag($tag:String) {
+  allContentfulRecipe(
+    sort: {fields: title, order: ASC}
+    filter: {content: {tags: {eq: $tag}}}
+  ) {
+    nodes {
+      id
+      title
+      prepTime
+      cookTime
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+}
+`
 export default TagTemplate
